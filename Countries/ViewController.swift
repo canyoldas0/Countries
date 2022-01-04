63c3d3fc132253919b81e15af0305a211e70c657
canyoldas <can.yoldas@huawei.com>
2022-01-04 10:08:14 +0300

    Initial Commit

---
+++ Countries/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  File name: ViewController.swift
+//  Description: <#Description#>
+//  Author: Can Yoldas (employee ID: c84203180) 
+//  Date Created: 4.01.2022
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
